860b43aa36096082368a3d1120752d36b17f4555
Tim Wang <y1wang86@gmail.com>
2017-05-02 20:28:16 -0400

    wrote basic code for location check

--- PokeYelp/app/src/main/java/pokeyelp/grat/team/pokemonyelp/activity_detail/DetailActivity.java
+++ PokeYelp/app/src/main/java/pokeyelp/grat/team/pokemonyelp/activity_detail/DetailActivity.java
@@ -1,6 +1,12 @@
 package pokeyelp.grat.team.pokemonyelp.activity_detail;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +14,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.gson.Gson;
 import com.koushikdutta.ion.Ion;
 
@@ -24,12 +35,17 @@ import pokeyelp.grat.team.pokemonyelp.gson_pokemon_species.Species;
 import pokeyelp.grat.team.pokemonyelp.gson_yelp.BusinessDetail;
 import pokeyelp.grat.team.pokemonyelp.singleton.MrSingleton;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-    private static final String FAKE_YELP_ID = "coffee-project-new-york-new-york";
     private String mYelpToken;
     private GetYelpBusiness mApiTask;
     private GetPokemon mPokemonTask;
+    private String mCurrentPokemon;
+    private Location mLocation;
+    private BusinessDetail mBusinessDetail;
+    private static final int REQUEST_LOCATION = 15;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +56,17 @@ public class DetailActivity extends AppCompatActivity {
         String yelpId = getIntent().getStringExtra(IntentCode.BUSINESS_ID_TO_DETAILS);
         mApiTask = new GetYelpBusiness();
         mApiTask.execute(Api.YELP_BASE_URL + "businesses/" + yelpId);
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     public void setupViews(BusinessDetail business){
+        mBusinessDetail = business;
         ((TextView) findViewById(R.id.detail_name)).setText(business.getName());
         ((TextView) findViewById(R.id.detail_adress)).setText(business.getLocation().getAddress1());
         findViewById(R.id.detail_button_scan_pokemon).setVisibility(View.VISIBLE);
@@ -58,6 +82,99 @@ public class DetailActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void showPokemon(){
+        if (mCurrentPokemon == null) {
+            Toast.makeText(DetailActivity.this, "No Pokemon Found!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(DetailActivity.this, mCurrentPokemon, Toast.LENGTH_SHORT).show();
+            ImageView pokemonSprite = (ImageView) findViewById(R.id.detail_pokemon_picture);
+            pokemonSprite.setVisibility(View.VISIBLE);
+            TextView catchButton = (TextView) findViewById(R.id.detail_button_catch_pokemon);
+            catchButton.setVisibility(View.VISIBLE);
+            String imageUrl = Pokemon.POKEMON_SPRITE_BASE_URL + mCurrentPokemon +".gif";
+            Ion.with(DetailActivity.this).load(imageUrl).intoImageView(pokemonSprite);
+            catchButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getLocation();
+                }
+            });
+        }
+    }
+
+
+    public void getLocation(){
+        if (!mGoogleApiClient.isConnected())
+            mGoogleApiClient.connect();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            mLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+
+            if (mLocation!=null) {
+                catchPokemon();
+            }else{
+                Toast.makeText(this, "Cannot determine location! Please try again", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } else {
+            ActivityCompat.requestPermissions(DetailActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        }
+    }
+
+    public void catchPokemon(){
+        Location pokemonLocation = new Location("");
+        pokemonLocation.setLatitude(mBusinessDetail.getCoordinates().getLatitude());
+        pokemonLocation.setLongitude(mBusinessDetail.getCoordinates().getLongitude());
+        Toast.makeText(this, "" + mLocation.distanceTo(pokemonLocation), Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        //getLocation();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        getLocation();
+
+                } else {
+                    Toast.makeText(this, "Cannot access your location", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocation = location;
+
     }
 
 
@@ -96,20 +213,10 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            if (s == null) {
-                Toast.makeText(DetailActivity.this, "No Pokemon Found!", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(DetailActivity.this, s, Toast.LENGTH_SHORT).show();
-                ImageView pokemonSprite = (ImageView) findViewById(R.id.detail_pokemon_picture);
-                pokemonSprite.setVisibility(View.VISIBLE);
-                String imageUrl = Pokemon.POKEMON_SPRITE_BASE_URL + s +".gif";
-//                Glide.with(DetailActivity.this).load(imageUrl)
-//                        .into(pokemonSprite);
-                Ion.with(DetailActivity.this).load(imageUrl).intoImageView(pokemonSprite);
-
-            }
+        protected void onPostExecute(String pokemonName) {
+            super.onPostExecute(pokemonName);
+            mCurrentPokemon = pokemonName;
+            showPokemon();
         }
     }
 
@@ -163,4 +270,6 @@ public class DetailActivity extends AppCompatActivity {
 
         }
     }
+
+
 }
