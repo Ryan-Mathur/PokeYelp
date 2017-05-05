1789fb33c3f98fe7bab58362cb36184454094cb8
Adrienne McCloud <mccloud.adrienne@gmail.com>
2017-05-05 11:06:23 -0400

    espresso test done

--- PokeYelp/app/src/androidTest/java/pokeyelp/grat/team/pokemonyelp/CollectionActivityTest.java
+++ PokeYelp/app/src/androidTest/java/pokeyelp/grat/team/pokemonyelp/CollectionActivityTest.java
@@ -20,12 +20,29 @@ public class CollectionActivityTest {
     @Rule
     public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<HomeActivity>(HomeActivity.class);
 
+    //user can move to the collection from the home screen and see pokemon caught.
     @Test
     public void testUserCanGoToCollection(){
 
         onView(withId(R.id.goto_collection_button)).perform(click());
-
         onView(withId(R.id.collection_recyclerview)).check(matches(isDisplayed()));
+    }
+
+    //user can move to the home screen from the collection activity
+    @Test
+    public void testUserCanGoToHomeFromCollection(){
+        onView(withId(R.id.goto_collection_button)).perform(click());
+        onView(withId(R.id.collection_recyclerview)).check(matches(isDisplayed()));
+        onView(withId(R.id.goto_home_button)).perform(click());
+
+    }
+    //user can move to the search activity from the collection(){
+    @Test
+    public void testUserCanMoveToSearchActivity(){
+        onView(withId(R.id.goto_collection_button)).perform(click());
+        onView(withId(R.id.collection_recyclerview)).check(matches(isDisplayed()));
+        onView(withId(R.id.go_to_search_button)).perform(click());
+        onView(withId(R.id.search_by_name)).check(matches(isDisplayed()));
     }
 
 
